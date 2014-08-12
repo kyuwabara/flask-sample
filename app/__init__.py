8703a80b7dc044760ea36addac72942bd6964f9e
kyuwabara <kyuwabara@gmail.com>
2014-08-12 12:31:10 +0900

    Loaded LoginManager.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from config import config
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -14,10 +17,15 @@ def create_app(config_name):
         pass
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .module_sample import module_sample as sample_blueprint
     app.register_blueprint(sample_blueprint)
     app.register_blueprint(sample_blueprint, url_prefix='/subdir')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
 
     return app
 
