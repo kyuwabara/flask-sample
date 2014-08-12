027c01f1f78ebfb05aceb1c356b6f089aa39057b
kyuwabara <kyuwabara@gmail.com>
2014-08-12 12:32:38 +0900

    Added adduser command.

--- manage.py
+++ manage.py
@@ -7,11 +7,29 @@ if os.path.exists('.env'):
         if len(kv) == 2:
             os.environ[kv[0].strip()] = kv[1].strip()
 
-from app import create_app
 from flask.ext.script import Manager
+
+from app import create_app, db
+from app.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+
+
+@manager.command
+def adduser(email):
+    from getpass import getpass
+    password = getpass()
+    password2 = getpass(prompt='Confirm:')
+    if password != password2:
+        import sys
+        sys.exit('passwords do not match')
+
+    db.create_all()
+    user = User(email=email, password=password)
+    user.save()
+    print('User {0} was registered.'.format(email))
+
 
 if __name__ == '__main__':
     manager.run()
