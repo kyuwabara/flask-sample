79026914d09e6d5908ff1b0211b17462302c5054
kyuwabara <kyuwabara@gmail.com>
2014-08-12 12:31:34 +0900

    Switched navbar link for login user.

--- app/module_sample/routes.py
+++ app/module_sample/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import get_flashed_messages, render_template
+from flask.ext.login import current_user
 
 from . import module_sample
 
 @module_sample.route('/')
 def index():
-    return render_template('skelton.html')
+    message = get_flashed_messages(False, ['message'])
+    email = 'guest'
+    return render_template('skelton.html', user=current_user, message=message)
 
